042449dc1036aeaa5189f3ef5f8aa26866f5c8aa
Goodya <1799972130@qq.com>
2021-04-25 21:42:56 +0800

    2021/4/25

--- demoone/src/DemoTwo.java
+++ demoone/src/DemoTwo.java
@@ -201,5 +201,24 @@ public class DemoTwo {
         return memo[target];
     }
 
+    /**
+     * 动态规划  爬楼梯  f(n) = f(n-1) + f(n-2)
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        if (n <= 1)
+            return 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+
+
 
 }
